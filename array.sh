88b643d26fa48bd79e8b5dab3d44a03feda885b1
Bhagavan <bsaladi@primetgi.com>
2021-12-29 18:16:31 +0530

    all files commited

--- array.sh
+++ array.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 ARRAY=(10,20,30)
 ARRAY[3]=100
 echo ${ARRAY[@]}
---
+++ input.sh
@@ -0,0 +1,5 @@
+echo $0  #Script Name
+echo $1  #Fiest arguemtn
+echo $2  #second arg
+echo $*  #All args
+echo $#  #total num of args
