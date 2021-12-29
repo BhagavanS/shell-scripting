ecdb54f74e8d8294e4fee3ad2501ad7b87878db5
Bhagavan <bsaladi@primetgi.com>
2021-12-29 14:01:42 +0530

    all files commited

---
+++ 01-shell.sh
@@ -0,0 +1,8 @@
+echo -e "\e[31mHello\n\tWorld\e[0m"
+echo "This is my colour"
+
+echo "What is your name?"
+read name
+echo "How are you $name"
+read remark
+echo "Glad to here you $name I am $remark"
