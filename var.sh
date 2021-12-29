5757d8a071bb3f8dc33d111fb7da1d3719246831
Bhagavan <bsaladi@primetgi.com>
2021-12-29 16:11:36 +0530

    all files commited

--- var.sh
+++ var.sh
@@ -1,9 +1,9 @@
 x=10
 echo $x
 
-DATE = "2021-12-29"
-echo "Today date is is $DATE"
+DATE='2021-12-29'
+echo Today date is is $DATE
 
-USERS = who | wc -l
+USERS =who | wc -l
 
-echo "Number of users login into the Linux machine = $USERS"
+echo "Number of users login into the Linux machine=$USERS"
